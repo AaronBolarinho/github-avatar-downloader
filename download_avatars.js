eaab30e380ec624d5b3a86c0599753ce4a7d0b2e
AaronBolarinho <aaron.bolarinho@gmail.com>
2018-10-02 00:18:35 +0000

    well done, got the list of images

--- download_avatars.js
+++ download_avatars.js
@@ -1,25 +1,28 @@
 var request = require('request');
-
-console.log('Welcome to the GitHub Avatar Downloader!');
+var secret = require('./secret.js');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
-      'AaronBolarinho': 'request'
-      'Authorization': 'token'
+      'User-Agent': 'request',
+      'Authorization': secret.GITHUB_TOKEN
     }
   };
 
   request(options, function(err, res, body) {
     cb(err, body);
+    var array = JSON.parse(body);
+    for (var i = 0; i < array.length; i++) {
+      console.log(array[i].avatar_url);
+    }
+    // console.log(array);
   });
 }
 
-/*
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+  // console.log("Errors:", err);
+  // console.log("Result:", result);
 });
-*/
 
+console.log('Welcome to the GitHub Avatar Downloader!');
